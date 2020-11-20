6bdd8ff93d5e20d2f5c76f6ebc059e7581f21327
Dilraj Somel <dsomel21@gmail.com>
2020-11-20 13:54:52 -0500

    Ref #2 Added fonts and prototyping the Prasang component

---
+++ src/components/Prasang/Prasang.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Prasang = props => {
+  return (
+    <>
+      <h2>{props.title}</h2>
+      <img src={props.imgUrl} />
+      <p>Prasang</p>
+    </>
+  );
+};
+
+export default Prasang;
---
+++ src/components/Prasang/Prasang.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Prasang from './Prasang';
+
+export default {
+  title: 'Prasang',
+  component: Prasang,
+};
+
+export const Chamkaur = () => (
+  <Prasang
+    title="The Battle of Chamkaur"
+    imgUrl="https://cdn130.picsart.com/273248779000201.jpg"
+  />
+);
