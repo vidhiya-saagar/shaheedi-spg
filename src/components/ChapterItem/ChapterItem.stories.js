4c17f32fdd1cf085ce37b1ee762e69917a38498b
Dilraj Somel <dsomel21@gmail.com>
2020-11-20 23:47:08 -0500

    Ref #4 Added the music player in the app

--- src/components/ChapterItem/ChapterItem.stories.js
+++ src/components/ChapterItem/ChapterItem.stories.js
@@ -9,7 +9,7 @@ export default {
 export const ChamkaurChapter1 = () => (
   <ChapterItem
     artwork="https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTcxNTA2MjAxMDc3NDkxMTcx/jesus-language-gettyimages-566423511.jpg"
-    number={41}
+    number={31}
     gurmukhiScript="AnMdpur CoVnw"
     name="Anandpur Chorrna"
   />
--- src/components/ChapterList/ChapterList.stories.js
+++ src/components/ChapterList/ChapterList.stories.js
@@ -10,21 +10,21 @@ const chamkaurChapters = [
   {
     artwork:
       'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e70e2478-3314-4a6b-a9c8-7fced70e6f31/dcheda2-d72bc290-f701-4b21-9e48-a34a8d834769.jpg/v1/fill/w_600,h_343,q_75,strp/anandpur_sahib_vector_art_by_damanpreetsinghkhurl_dcheda2-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0zNDMiLCJwYXRoIjoiXC9mXC9lNzBlMjQ3OC0zMzE0LTRhNmItYTljOC03ZmNlZDcwZTZmMzFcL2RjaGVkYTItZDcyYmMyOTAtZjcwMS00YjIxLTllNDgtYTM0YThkODM0NzY5LmpwZyIsIndpZHRoIjoiPD02MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.Z1TEp5rNJJUXiUhPt9ExvmC3bAr3MGHI65I4O9q13FQ',
-    number: 41,
+    number: 31,
     gurmukhiScript: 'AnMdpur CoVnw',
     name: 'Anandpur Chorrna',
   },
   {
     artwork:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzkD7Fr38S5p9PGO71L0kPojLUHp2HcoPSow&usqp=CAU',
-    number: 42,
+    number: 32,
     gurmukhiScript: 'iphwrI rwjy',
     name: 'Pihari Raaje',
   },
   {
     artwork:
       'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/00714226-88e5-48cd-8b5e-736f73e06f76/dcnjzs9-e5c73dae-ce30-4a14-9d7d-ea261ced1f3a.png/v1/fill/w_1024,h_1505,q_80,strp/guru_gobind_singh_ji_l_digital_art_by_ms_saluja_by_ms_saluja_dcnjzs9-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xNTA1IiwicGF0aCI6IlwvZlwvMDA3MTQyMjYtODhlNS00OGNkLThiNWUtNzM2ZjczZTA2Zjc2XC9kY25qenM5LWU1YzczZGFlLWNlMzAtNGExNC05ZDdkLWVhMjYxY2VkMWYzYS5wbmciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.wfAlqj89jLGU4GjgEnPg5bh4GVB2lzCqfZQVKMfOT7Q',
-    number: 43,
+    number: 33,
     gurmukhiScript: 'Akwl',
     name: 'Akaaaal',
   },
