8bf6ae6953cd5de4c83fd9f01764ccf23415872c
Dilraj Somel <dsomel21@gmail.com>
2020-12-22 11:15:23 -0500

    Can't figure out how to make the button "stay" at the bottom :(

--- src/screens/ChapterIndexScreen/ChapterIndexScreen.js
+++ src/screens/ChapterIndexScreen/ChapterIndexScreen.js
@@ -4,6 +4,9 @@ import Grid from '../../components/Grid';
 import ChapterListStyles from '../../components/ChapterList/ChapterList.module.css';
 import { fetchGet } from '../../helper/fetchHelper';
 import ChapterList from '../../components/ChapterList';
+import { Link } from 'react-router-dom';
+import CornerButton from '../../components/CornerButton';
+import CornerButtonStyles from '../../components/CornerButton/CornerButtonStyles.module.css';
 
 const ChapterIndexScreen = () => {
   const [chamkaurChapters, setChamkaurChapters] = useState(null);
@@ -26,18 +29,39 @@ const ChapterIndexScreen = () => {
         customClass={isDarkMode ? `${Styles.DarkMode}` : ''}
       >
         <Grid column={true} sm={12} md={12} lg={10}>
-          <h3 className={Styles.PageTitle}>Rut 6 - Chapters</h3>
-        </Grid>
+          <Grid alignItems="center" justify="center">
+            <Grid column={true} sm={12} md={12} lg={10}>
+              <h3 className={Styles.PageTitle}>Rut 6 - Chapters</h3>
+            </Grid>
 
-        {/* ChapterLists (X-Scroll on Mobile) */}
+            {/* ChapterLists (X-Scroll on Mobile) */}
+            <Grid
+              column={true}
+              sm={12}
+              md={12}
+              lg={10}
+              customClass={isDarkMode ? `${Styles.DarkMode}` : ''}
+            >
+              <Grid
+                alignItems="baseline"
+                justify="flex-start"
+                customClass={ChapterListStyles.Flow}
+              >
+                <ChapterList chapters={chamkaurChapters} />
+              </Grid>
+            </Grid>
+          </Grid>
 
-        <Grid column={true} sm={12} md={12} lg={10}>
-          <Grid
-            alignItems="baseline"
-            justify="flex-start"
-            customClass={ChapterListStyles.Flow}
-          >
-            <ChapterList chapters={chamkaurChapters} />
+          <Grid alignItems="center" justify="center">
+            <Grid column={true} sm={12} md={8} lg={6}>
+              <Link to={`/`}>
+                <button
+                  className={`${CornerButtonStyles.Orange} ${CornerButtonStyles.BottomLeft}`}
+                >
+                  Home
+                </button>
+              </Link>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
