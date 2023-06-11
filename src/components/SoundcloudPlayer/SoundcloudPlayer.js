d3455f62fdc9531744b41977e00f97485f762af4
Dilraj Somel <dsomel21@gmail.com>
2023-06-11 13:11:33 -0400

    Refactor SoundcloudPlayer component

--- src/components/SoundcloudPlayer/SoundcloudPlayer.js
+++ src/components/SoundcloudPlayer/SoundcloudPlayer.js
@@ -2,38 +2,19 @@ import React, { useState, useEffect } from 'react';
 import ReactPlayer from 'react-player/soundcloud';
 import Styles from './SoundcloudPlayer.module.css';
 
-const SoundcloudPlayer = () => {
-  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 40 * 16);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsLargeScreen(window.innerWidth >= 40 * 16);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    // Cleanup event listener on component unmount
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+const SoundcloudPlayer = ({trackUrl="https://soundcloud.com/vidhiya-saagar/sets/rut-1-chapter-1"}) => {
+  const src = `https://w.soundcloud.com/player/?url=${encodeURIComponent(trackUrl)}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true&show_artwork=false`;
 
   return (
-    <div className={Styles.Container}>
-<ReactPlayer
-  className={Styles.Player}
-  url="https://soundcloud.com/vidhiya-saagar/sets/rut-1-chapter-1"
-  height={isLargeScreen ? '400px' : '140px'}
-  width={isLargeScreen ? '50%' : '100%'}
-  config={{
-    soundcloud: {
-      options: {
-        showArtwork: false
-      }
-    }
-  }}
-
-/>
+    <div className={Styles.PlayerContainer}>
+      <iframe
+        width="100%"
+        height="100%"
+        scrolling="no"
+        frameborder="no"
+        allow="autoplay"
+        src={src}
+      />
     </div>
   );
 };
